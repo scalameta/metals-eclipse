d40be248c3c0013fcb5c3a47532c9fcc77a1a3e2
Edmundo LOPEZ BOBEDA <edmundo@lopezbobeda.net>
2019-09-09 05:13:38 -0400

    Add slow task support.

--- lsp-scala-plugin/src/main/scala/com/idiomaticsoft/lsp/scala/metals/MetalsLanguageClient.scala
+++ lsp-scala-plugin/src/main/scala/com/idiomaticsoft/lsp/scala/metals/MetalsLanguageClient.scala
@@ -2,11 +2,17 @@ package com.idiomaticsoft.lsp.scala.metals
 
 import org.eclipse.lsp4j.services.LanguageClient
 import org.eclipse.lsp4j.jsonrpc.services.JsonNotification
+import org.eclipse.lsp4j.jsonrpc.services.JsonRequest
+import java.util.concurrent.CompletableFuture
+
 
 trait MetalsLanguageClient extends LanguageClient {
 	
 	
 	@JsonNotification("metals/status")
 	def metalsStatus(status: MetalsStatusParams): Unit
+
+	@JsonRequest("metals/slowTask")
+	def metalsSlowTask(status: MetalsSlowTaskParams): CompletableFuture[MetalsSlowTaskResult]
 
 }
