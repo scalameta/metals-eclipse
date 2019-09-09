211f81824743c16466db87506a9b7d80eae25707
Edmundo LOPEZ BOBEDA <edmundo@lopezbobeda.net>
2019-09-09 05:33:07 -0400

    Add force to tag

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -8,11 +8,11 @@ NEW_VERSION=${ARGV[1]}
 
 if [ $ARGC -eq 2 ]; then
   cd metals-eclipse/parent
-  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$RELEASE_VERSION
+  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$RELEASE_VERSION -f parent/pom.xml
   git commit -a -m "Bump version to $NEW_VERSION"
   git tag -a v$RELEASE_VERSION
-  git push --tags origin master
-  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$NEW_VERSION
+  git push --tags --force origin master
+  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$NEW_VERSION -f parent.pom.xml 
   git commit -a -m "Bump version to $NEW_VERSION-SNAPSHOT"
   git push origin master
 else
