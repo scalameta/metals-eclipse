a0a4d49d8ed9fc6c14330762ab198358ef142eb2
Edmundo Lopez Bobeda <edmundo@lopezbobeda.net>
2019-09-13 17:46:02 -0400

    Fix bumpVersion script

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -12,7 +12,7 @@ if [ $ARGC -eq 2 ]; then
   git commit -a -m "Bump version to $NEW_VERSION"
   git tag -a v$RELEASE_VERSION
   git push --tags --force origin master
-  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$NEW_VERSION -f parent/pom.xml 
+  mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=$NEW_VERSION-SNAPSHOT -f parent/pom.xml 
   git commit -a -m "Bump version to $NEW_VERSION-SNAPSHOT"
   git push origin master
 else
