79601d20d5aa69c69a357d56b43e7ee807b5ca03
Edmundo Lopez Bobeda <edmundo@lopezbobeda.net>
2019-10-03 17:35:13 -0400

    Add copyright notice

--- lsp-scala-plugin/src/main/scala/com/idiomaticsoft/lsp/scala/metals/MetalsLaunchConfigurationDelegate.scala
+++ lsp-scala-plugin/src/main/scala/com/idiomaticsoft/lsp/scala/metals/MetalsLaunchConfigurationDelegate.scala
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Idiomaticsoft S.R.L. and others.
+ * This program and the accompanying materials are made
+ * available under the terms of the APACHE LICENSE, VERSION 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Contributors:
+ *  Edmundo Lopez B. (Idiomaticsoft S.R.L.) - initial implementation
+ *******************************************************************************/
+
 package com.idiomaticsoft.lsp.scala.metals
 
 import org.eclipse.jdt.launching.JavaLaunchDelegate
